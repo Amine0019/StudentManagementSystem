938de9cdc476e97bd5af7fa8812795a048b0e359
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2024-12-26 18:11:43 +0100

    Student class Statement

---
+++ src/Etudiant.java
@@ -0,0 +1,8 @@
+public class Etudiant {
+    // Les Attributs de la classe
+    String nom ;
+    String prenom ;
+    float [] tab_notes = new float[10]; // Tableau contenant les 10 notes de l'etudiant initialisées a 0
+
+
+}
